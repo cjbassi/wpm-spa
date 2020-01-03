85cd9d2069202ccc1c05c921a3cff0f81c5c1c6c
Caleb Bassi <calebjbassi@gmail.com>
2020-01-02 17:53:31 -0800

    Updates

---
+++ precache-manifest.b99a128705776aa4c02da71c9bc9e8f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d15387d81f901f6399b",
+    "url": "/wpm-spa/static/js/main.6d15387d.chunk.js"
+  },
+  {
+    "revision": "bcb49e10c4b413119b00",
+    "url": "/wpm-spa/static/js/1.bcb49e10.chunk.js"
+  },
+  {
+    "revision": "0ef47870ed2a969b8dc2",
+    "url": "/wpm-spa/static/js/runtime~main.0ef47870.js"
+  },
+  {
+    "revision": "d16280d347da304ec7739fc887321f00",
+    "url": "/wpm-spa/index.html"
+  }
+];
